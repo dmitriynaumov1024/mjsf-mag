eeaa0245adfc853fc21300bf532974cea7d30691
Dmitriy Naumov <naumov1024@gmail.com>
2024-09-15 11:26:08 +0300

    Vue.js lab 1

---
+++ lab01/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+let keys = [
+  "name", "group", "speciality"
+]
+
+let me = {
+  name: "Дмитро Наумов",
+  group: "8.1213-іпз",
+  speciality: "інженерія програмного забезпечення"
+}
+
+let locale = {
+  name: "ім'я",
+  group: "група",
+  speciality: "спеціальність"
+}
+
+let greeting = "Hell to world!"
+</script>
+
+<template>
+  <h3>{{greeting}}</h3>
+  <table>
+    <template v-for="key in keys">
+      <tr>
+        <td>{{locale[key]}} :</td>
+        <td>{{me[key]}}</td>
+      </tr>
+    </template>
+  </table>
+</template>
